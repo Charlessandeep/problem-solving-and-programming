96f6ec396feb5718d0ab209a5f15813cda8ddcc4
Charlessandeep <51992238+Charlessandeep@users.noreply.github.com>
2019-06-29 12:38:38 +0530

    Add files via upload

---
+++ control flow statements.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x=10;
+if(x>15):
+    print("hello")
+else:
+    print("bye")
+    
+
+
+# 
+# 
+
+# In[2]:
+
+
+x=10;
+y=20;
+if(x>y):
+    print ("x is greater number");
+else :
+    print ("y is greater number");
+    
+
+
+# In[3]:
+
+
+x=6;
+y=2;
+if(x==y):
+    print(x**x);
+else:
+        print(x*y);
+    
+
+
+# In[4]:
+
+
+x=0;
+if(x<0):
+    print("it is a negative number");
+elif(x>0):
+    print("it is a positive number");
+elif(x==0):
+    print("the number is zero")
+
+
+# In[5]:
+
+
+n=1;
+while(n<=10):
+    print(n)
+    n=n+1;
+
+
+# In[6]:
+
+
+n=10;
+while(n>=1):
+    print(n)
+    n=n-1;
+    
+
+
+# In[ ]:
+
+
+x=int(input("enter lower limit"));
+y=int(input("enter upper limit"));
+sum=0;
+while(x!=0):
+    if(x%2==0):
+        sum=sum+x;
+    x=x+1;
+print(sum);
+
+
+# In[ ]:
+
+
+
+
